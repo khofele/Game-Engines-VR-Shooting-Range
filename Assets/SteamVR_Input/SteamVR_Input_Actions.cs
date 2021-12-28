72627ff8e39879a2538f7d1e37968ae5345c81cf
Karen Hofele <karen.hofele@gmx.de>
2021-12-28 22:29:49 +0100

    [added] shoot action + sprite [fixed] minor stuff at weapon prefab

--- Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -47,6 +47,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Boolean p_default_Climb;
         
+        private static SteamVR_Action_Boolean p_default_Shoot;
+        
         private static SteamVR_Action_Vibration p_default_Haptic;
         
         private static SteamVR_Action_Vector2 p_platformer_Move;
@@ -183,6 +185,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean default_Shoot
+        {
+            get
+            {
+                return SteamVR_Actions.p_default_Shoot.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         public static SteamVR_Action_Vibration default_Haptic
         {
             get
@@ -265,6 +275,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_Trackpad,
                     SteamVR_Actions.default_GrapplingHoook,
                     SteamVR_Actions.default_Climb,
+                    SteamVR_Actions.default_Shoot,
                     SteamVR_Actions.default_Haptic,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
@@ -289,6 +300,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_Trackpad,
                     SteamVR_Actions.default_GrapplingHoook,
                     SteamVR_Actions.default_Climb,
+                    SteamVR_Actions.default_Shoot,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -314,6 +326,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_Jump,
                     SteamVR_Actions.default_GrapplingHoook,
                     SteamVR_Actions.default_Climb,
+                    SteamVR_Actions.default_Shoot,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset};
@@ -341,6 +354,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_Trackpad,
                     SteamVR_Actions.default_GrapplingHoook,
                     SteamVR_Actions.default_Climb,
+                    SteamVR_Actions.default_Shoot,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -366,6 +380,7 @@ namespace Valve.VR
             SteamVR_Actions.p_default_Trackpad = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/default/in/Trackpad")));
             SteamVR_Actions.p_default_GrapplingHoook = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/GrapplingHoook")));
             SteamVR_Actions.p_default_Climb = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/Climb")));
+            SteamVR_Actions.p_default_Shoot = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/Shoot")));
             SteamVR_Actions.p_default_Haptic = ((SteamVR_Action_Vibration)(SteamVR_Action.Create<SteamVR_Action_Vibration>("/actions/default/out/Haptic")));
             SteamVR_Actions.p_platformer_Move = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/platformer/in/Move")));
             SteamVR_Actions.p_platformer_Jump = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/platformer/in/Jump")));
