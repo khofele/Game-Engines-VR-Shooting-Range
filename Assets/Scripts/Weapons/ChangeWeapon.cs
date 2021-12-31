8181558b9b8af6d1d0086982d90db3171dd31df0
Karen Hofele <karen.hofele@gmx.de>
2021-12-31 11:21:26 +0100

    [added] weapons to scene + change weapon skript

---
+++ Assets/Scripts/Weapons/ChangeWeapon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ChangeWeapon : MonoBehaviour
+{
+    [SerializeField] private GameObject rightHand = null;
+    private GameObject newWeapon = null;
+    private GameObject currentWeapon = null;
+
+    private void Start()
+    {
+        currentWeapon = rightHand.GetComponent<Hand>().renderModelPrefab.gameObject.GetComponent<RenderModel>().controllerPrefab.gameObject;
+    }
+
+    private void ChangeCurrentWeapon()
+    {
+        if(newWeapon != currentWeapon)
+        {
+            rightHand.GetComponent<Hand>().renderModelPrefab.gameObject.GetComponent<RenderModel>().controllerPrefab = newWeapon;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Weapon")
+        {
+            newWeapon = other.gameObject;
+            ChangeCurrentWeapon();
+        }
+    }
+}
